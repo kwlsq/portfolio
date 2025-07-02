3d38d65346d87583e8675da72e0a847ee61ab8a3
Vincentius <vincentiussss@gmail.com>
2025-07-02 22:06:58 +0700

    feat: add icon and component files

---
+++ src/app/components/TechStacksSection.tsx
@@ -0,0 +1,7 @@
+export const TechStacksSection = () => {
+  return (
+    <section className="">
+      <h2 className="text-4xl font-bold text-center my-8">My Tech Stacks</h2>
+    </section>
+  );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { HeroSection } from "./components/HeroSection";
+import { TechStacksSection } from "./components/TechStacksSection";
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
         Website is under construction.
       </div>
       <HeroSection />
+      <TechStacksSection />
     </main>
   );
 }
